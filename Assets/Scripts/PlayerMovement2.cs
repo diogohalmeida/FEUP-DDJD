af5d19d9bc97d9ed4e0a4a0c5207133f94e1c08a
CoDyPhin <carlosdaniel.rl@gmail.com>
2022-03-15 21:46:48 +0000

    Updated and added a bunch of assets, added running animations and included them in the state machine for both main characters

---
+++ Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+
+    Rigidbody2D body;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        //positions = GetComponentsInChildren<Transform>();
+        //n_players = bodies.Length;
+        //body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            body.AddForce(new Vector2(0, 8), ForceMode2D.Force);
+        }
+        /*if (Input.GetKey(KeyCode.W)){
+            body.AddForce(new Vector2(0, 8), ForceMode2D.Force);
+        }*/
+        if (transform.position.y > -3.114){
+            animator.SetBool("is_flying", true);
+        }
+        else {
+            animator.SetBool("is_flying", false);
+        }
+    }
+}
