d92910bb345e7b9f3c1a507c5dfa4526e0ba44ae
supertommc <correiodotomas@gmail.com>
2022-04-03 11:15:46 +0100

    Fixed volume sliders and some sounds

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
@@ -10,15 +11,34 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField]
     private AudioMixer soundEffectsMixer;
 
+    [SerializeField]
+    private Slider musicSlider;
+
+    [SerializeField]
+    private Slider effectsSlider;
+
+    float musicVolume = 1f;
+    float effectsVolume = 1f;
+
+    void Start()
+    {
+        musicVolume = PlayerPrefs.GetFloat("musicVolume");
+        effectsVolume = PlayerPrefs.GetFloat("effectsVolume");
+        musicSlider.value = musicVolume;
+        effectsSlider.value = effectsVolume;
+    }
+
 
     public void SetMusicVolume(float sliderValue)
     {
         musicMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("musicVolume", sliderValue);
     }
 
     public void SetEffectsVolume(float sliderValue)
     {
         soundEffectsMixer.SetFloat("SoundEffectsVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("effectsVolume", sliderValue);
     }
 
 
